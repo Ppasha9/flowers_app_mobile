d5373db17df883bdedd478b5dc571709d08a87ce
Ppasha9 <pavelvan50@gmail.com>
2021-05-09 23:58:07 +0300

    Delete unused files of old version of application

--- flowers_app/android/app/src/main/kotlin/com/example/flowers_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flowers_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flowers_app/lib/languages/constants.dart
+++
@@ -1,9 +0,0 @@
-class LanguageConstants {
-  static const Map<String, String> _fromEngToRus = {
-    "New": "Новинки",
-    "Bouquet": "Букеты",
-    "Basket": "Корзинки",
-  };
-
-  static String fromEngToRus(String eng) => _fromEngToRus[eng];
-}
